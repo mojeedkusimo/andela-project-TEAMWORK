17e827ac5ddc2f03f631d2eacefaba53045a47dd
Mojeed Kusimo <mkusimo90@gmail.com>
2021-02-04 06:40:21 +0100

    Added db connection link for windows machine

--- db/index.js
+++ db/index.js
@@ -1,7 +1,8 @@
 const { Client } = require("pg");
 
 const client = new Client({
-    connectionString: "postgresql:mojeedkusimo:root@localhost/andela-teamwork"
+    // connectionString: "postgresql:mojeedkusimo:root@localhost/andela-teamwork" //linux connection
+    connectionString: "postgresql:mojeedkusimo:root@localhost/andela_teamwork" //windows connection
 });
 
 client.connect();
