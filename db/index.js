e176af6dc946b25d15dcd665fc1eb1b941b11b06
MOJEED KUSIMO <mkusimo90@gmail.com>
2021-02-13 12:19:07 +0100

    Still troubleshooting

--- db/index.js
+++ db/index.js
@@ -1,10 +1,10 @@
 const { Client } = require("pg");
 require("dotenv").config();
 const LOCAL_DB_CON = process.env.LOCAL_DB_CON;
-const DATABASE_URL = process.env.DATABASE_URL;
+// const DATABASE_URL = process.env.DATABASE_URL;
 
 const client = new Client({
-    connectionString: DATABASE_URL || LOCAL_DB_CON
+    connectionString: process.env.DATABASE_URL || LOCAL_DB_CON
 });
 
 client.connect();
