7867effc9f0d0e82029bf34ffb861d46ee97ec93
MOJEED KUSIMO <mkusimo90@gmail.com>
2021-02-13 08:51:40 +0100

    Corrected database connection string

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 const { Client } = require("pg");
 require("dotenv").config();
 const LOCAL_DB_CON = process.env.LOCAL_DB_CON;
+const DATABASE_URL = process.env.DATABASE_URL;
 
 const client = new Client({
     connectionString: LOCAL_DB_CON || DATABASE_URL
