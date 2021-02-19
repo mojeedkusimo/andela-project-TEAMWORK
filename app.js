dbdc12f4b95fc51cb569c7f34b3ff38da55f34c1
MOJEED KUSIMO <mkusimo90@gmail.com>
2021-02-19 22:05:29 +0100

    Added cors module

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const routes = require("./routes");
 require("dotenv").config();
+const cors = require("cors");
 
 const PORT = process.env.PORT || 5000
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
